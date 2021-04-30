fdff105ef0c308161a51fe570e5411410a556475
JeanEric <ericjohn415@gmail.com>
2021-05-01 00:28:37 +0200

    used function_based api

--- api_basic/serializers.py
+++ api_basic/serializers.py
@@ -1,21 +1,10 @@
 from rest_framework import serializers
 from .models import Article
 
-class ArticleSerializer(serializers.Serializer):
-    title = serializers.CharField(max_length=100)
-    author = serializers.CharField(max_length=100)
-    email = serializers.EmailField(max_length=100)
-    date = serializers.DateTimeField()
+class ArticleSerializer(serializers.ModelSerializer):
 
-    def create(self, validated_data):
-        return Article.objects.create(validated_data)
+    class Meta:
+        model = Article
+        fields = ['id', 'title', 'author']
 
-
-    def update(self, instance, validated_data):
-        instance.title = validated_data.get('title', instance.title)
-        instance.author = validated_data.get('author', instance.author)
-        instance.email = validated_data.get('email', instance.email)
-        instance.date = validated_data.get('date', instance.date)
-        instance.save()
-        return instance
         
--- api_basic/views.py
+++ api_basic/views.py
@@ -1,3 +1,52 @@
 from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+from rest_framework.parsers import JSONParser
+from rest_framework import status
+from django.views.decorators.csrf import csrf_exempt
+from .models import Article
+from .serializers import ArticleSerializer
 
 # Create your views here.
+
+@csrf_exempt
+def article_list(request):
+    if request.method == 'GET':
+        articles = Article.objects.all()
+        serializer = ArticleSerializer(articles, many=True)
+        return JsonResponse(serializer.data, safe=False)
+
+    elif request.method == 'POST':
+        data = JSONParser().parse(request)
+        serializer = ArticleSerializer(data=data)
+        
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=status.HTTP_201_CREATED)
+        return JsonResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST)    
+
+
+@csrf_exempt
+def article_detail(request, pk):
+    try:
+        article = Article.objects.get(pk=pk)
+
+    except Article.DoesNotExist:
+        return HttpResponse(status=404)
+
+    if request.method == 'GET':
+        serializer = ArticleSerializer(article)
+        return JsonResponse(serializer.data)
+
+    elif request.method == 'PUT':
+
+        data = JSONParser().parse(request)
+        serializer = ArticleSerializer(article, data=data)
+        
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data)
+        return JsonResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST) 
+
+    elif request.method == 'DELETE':
+        article.delete()
+        return HttpResponse(status=status.HTTP_204_NO_CONTENT)
